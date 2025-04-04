a26d3ed683eb2c62e47457d5b5f352342db4df16
tapader13 <minhajtapader0@gmail.com>
2025-04-03 21:13:18 -0400

    changes in page

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,7 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <div>
-      <p>Home Page</p>
-      <Button> Click me </Button>
+      <Articles />
     </div>
   );
 }
