887f213899c367f0bd15d7fe8187919b3002848b
tapader13 <minhajtapader0@gmail.com>
2025-04-03 22:46:45 -0400

    delete custom compont folder

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import OurBlog from '@/components/custom-components/our-blog/OurBlog';
-import CTA from '@/components/custom_component/cta/CTA';
+import CTA from '@/components/custom-components/cta/CTA';
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
