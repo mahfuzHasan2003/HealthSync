dcc3bc7d4f5777b0ef3327b3250dcae65c46a1a2
mahfuzHasan2003 <mahfuzhasan2003@yahoo.com>
2025-04-05 04:17:16 +0600

    solved conflict issue

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import OurBlog from '@/components/custom-components/our-blog/OurBlog';
 import CTA from '@/components/custom-components/cta/CTA';
 import Footer from '@/components/custom-components/footer/Footer';
 import Available from '@/components/custom-components/available/Available';
-import OurPortfolio from '@/components/custom-components/oufr-portfolio/OurPortfolio';
+import OurPortfolio from '@/components/custom-components/our-portfolio/OurPortfolio';
 import OurTeam from '@/components/custom-components/our-team/our-team';
 import HeroSlider from '@/components/custom-components/hero-section/hero-slider';
 
