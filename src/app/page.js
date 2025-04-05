a3067f8885891cd2b2b0669061f1d20a5fcf06a6
tapader13 <minhajtapader0@gmail.com>
2025-04-04 23:03:45 -0400

    our portfolio part design complete

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,12 @@ import CTA from '@/components/custom-components/cta/CTA';
 import { Button } from '@/components/ui/button';
 import Footer from '@/components/custom-components/footer/Footer';
 import Available from '@/components/custom-components/available/Available';
+import OurPortfolio from '@/components/custom-components/oufr-portfolio/OurPortfolio';
 
 export default function Home() {
   return (
     <div>
+      <OurPortfolio />
       <Available />
       <CTA />
       <OurBlog /> <Footer />
